22bacb014c9197a060385c5432970546e2a93612
DaromanX <developerdaro@gmail.com>
2024-08-16 11:57:36 -0500

    iniciando kotlin

---
+++ Estructuras de control/condicionales.kt
@@ -0,0 +1,7 @@
+/*
+Descripción: Escribe un programa que pida al usuario ingresar un número entero y determine si es positivo, negativo o cero.
+
+Requisitos:
+    Utiliza una estructura if-else para evaluar la condición.
+    Muestra un mensaje indicando si el número es positivo, negativo o cero.
+*/
